4ded1ce9b1fe533bfd152161fd23a735441828c5
nattygirma <nattygirma28@gmail.com>
2025-05-05 11:54:33 -0400

    first commit

--- api/predict.js
+++ api/predict.js
@@ -2,7 +2,7 @@
 const apiUrl = import.meta.env.VITE_API_URL; 
 console.log("apiUrl_222", apiUrl);
 export default async function handler(req, res) {
-    const response = await fetch(apiUrl + '/predict', {
+    const response = await fetch('http://3.89.212.31:80/predict', {
       method: req.method,
       headers: {
         ...req.headers,
