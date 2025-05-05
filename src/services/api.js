5e898b58e752b1ddd63a07dcfff74afa13da5b32
nattygirma <nattygirma28@gmail.com>
2025-05-05 11:23:44 -0400

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.VITE_MOVIE_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
-
+const API_URL = import.meta.env.VITE_API_URL;
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${Math.floor(Math.random() * 10) + 1}`);
   const data = await response.json();
@@ -16,4 +16,13 @@ export const searchMovies = async (query) => {
   );
   const data = await response.json();
   return data.results;
+};
+
+export const predictGenre = async (movie) => {
+  const response = await fetch(`${API_URL}/predict`, {
+    method: 'POST',
+    body: JSON.stringify(movie),
+  });
+  const data = await response.json();
+  return data;
 };
